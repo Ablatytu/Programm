3c9a53579f4bb349ad0512d09d3e006dcd564b67
Ablatytu <stassuverin@gmail.com>
2023-12-07 22:54:00 +0300

    Задание

---
+++ Задание 1/Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program1
+{
+    static void Main()
+    {
+        // Пример 
+        int N = 5;
+        Console.Write($"N = {N} -> ");
+        PrintNaturalNumbers(N);
+
+        N = 8;
+        Console.Write($"\nN = {N} -> ");
+        PrintNaturalNumbers(N);
+    }
+
+    static void PrintNaturalNumbers(int n)
+    {
+        if (n <= 0)
+            return;
+        
+        Console.Write($"{n}, ");
+        PrintNaturalNumbers(n - 1);
+    }
+}
---
+++ Задание 3/Program3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program3
+{
+    static void Main()
+    {
+        // Пример 
+        int m1 = 2, n1 = 3;
+        int result1 = AckermannFunction(m1, n1);
+        Console.WriteLine($"A({m1},{n1}) = {result1}");
+
+        int m2 = 3, n2 = 2;
+        int result2 = AckermannFunction(m2, n2);
+        Console.WriteLine($"A({m2},{n2}) = {result2}");
+    }
+
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (m > 0 && n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
+}
